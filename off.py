42556f984c3841c78813f498fa4b589d34b6643c
Ki-re <erikbatiste10@gmail.com>
2023-07-13 11:17:15 +0200

    V2.7

--- off.py
+++ off.py
@@ -6,13 +6,11 @@ for ip in ip_list:
     iteration = 0
     while True:
         iteration += 1
-        requests.get(f"http://{ip}:5000/on")
+        requests.get(f"http://{ip}:5000/off")
         time.sleep(3)
-        requests.get(f"http://{ip}:5000/as")
-        time.sleep(5)
         status = requests.get(f"http://{ip}:5000/status").text
         status = status.split()
-        if "on" in status or iteration >= 3:
+        if "standby" in status or iteration >= 3:
             break
         else:
             time.sleep(5)
