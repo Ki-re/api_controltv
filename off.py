f6aa6dafa6e4622efa25f757482a055a1758b56f
Ki-re <erikbatiste10@gmail.com>
2023-07-13 13:26:56 +0200

    V2.8

--- off.py
+++ off.py
@@ -3,14 +3,19 @@ import requests
 import time
 
 for ip in ip_list:
+    print(f"- IP {ip}")
     iteration = 0
     while True:
         iteration += 1
         requests.get(f"http://{ip}:5000/off")
+        print("request apagar")
         time.sleep(3)
         status = requests.get(f"http://{ip}:5000/status").text
+        print(f"status: {status}")
         status = status.split()
-        if "standby" in status or iteration >= 3:
+        if "standby" in status or "unknown" in status or iteration >= 3:
+            print("saliendo")
             break
         else:
+            print("volvemos a intentarlo")
             time.sleep(5)
--- on.py
+++ on.py
@@ -3,16 +3,22 @@ import requests
 import time
 
 for ip in ip_list:
+    print(f"- IP {ip}")
     iteration = 0
     while True:
         iteration += 1
         requests.get(f"http://{ip}:5000/on")
-        time.sleep(3)
+        print("request encender")
+        time.sleep(5)
         requests.get(f"http://{ip}:5000/as")
+        print("request seteado el active source")
         time.sleep(5)
         status = requests.get(f"http://{ip}:5000/status").text
+        print(f"status: {status}")
         status = status.split()
         if "on" in status or iteration >= 3:
+            print("saliendo")
             break
         else:
+            print("volvemos a intentarlo")
             time.sleep(5)
