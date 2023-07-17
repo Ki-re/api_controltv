fc1ccee0ddba044701faeb532e2a93e71ca6f09a
Ki-re <erikbatiste10@gmail.com>
2023-07-17 13:54:52 +0200

    Update app_raspb.py

--- app_raspb.py
+++ app_raspb.py
@@ -4,15 +4,16 @@ from flask import Flask, request
 from waitress import serve
 import subprocess
 import werkzeug.serving
-import time
 
 # Para que se encienda chromium con la raspi hay que poner lo siguiente
 # @sensible-browser IP --start-fullscreen 
 # En la ruta: /etc/xdg/lxsession/LXDE-pi/autostart
 
-while True:
-        if subprocess.check_output("hostname -I", shell=True) != "":
-                break
+# while True:
+#         if subprocess.check_output("hostname -I", shell=True) != "":
+#                 break
+        
+print(subprocess.check_output("hostname -I", shell=True))
         
 app = Flask(__name__)
 user = subprocess.check_output("echo $USER", shell=True) # Obtenemos el username
