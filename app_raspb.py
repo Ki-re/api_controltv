9d8f85efa5a3abd3ff2d0050952f9d3a343dff86
Ki-re <erikbatiste10@gmail.com>
2025-02-24 10:36:03 +0100

    Files Refactor
    - Translated comments
    - Removed unnecessary files
    - Restructured file organization

--- app_raspb.py
+++ app_raspb.py
@@ -6,50 +6,50 @@ import subprocess
 import werkzeug.serving
 import time
 
-# Para que se encienda chromium con la raspi hay que poner lo siguiente
-# @sensible-browser IP --start-fullscreen 
-# En la ruta: /etc/xdg/lxsession/LXDE-pi/autostart
+# To launch Chromium on the Raspberry Pi, add the following:
+# @sensible-browser IP --start-fullscreen  
+# In the path: /etc/xdg/lxsession/LXDE-pi/autostart  
 
 time.sleep(30)
                 
 app = Flask(__name__)
-user = subprocess.check_output("echo $USER", shell=True) # Obtenemos el username
-ip = ((str(((subprocess.check_output("hostname -I", shell=True)).split())[-1])).replace("b", "")).replace("'", "") # Obtenemos la IP y limpiamos el output 
+user = subprocess.check_output("echo $USER", shell=True) # Get the username  
+ip = ((str(((subprocess.check_output("hostname -I", shell=True)).split())[-1])).replace("b", "")).replace("'", "") # Get the IP and clean the output  
 run_port = 5000
 
-# Uso: X.X.X.X:5000/on
-@app.route('/on') # Enciende el televisor por medio del cec
+# Usage: X.X.X.X:5000/on
+@app.route('/on') # Turns on the TV using CEC  
 def on():
         os.system('echo "on 0" | cec-client -s -d 1')
-        return "Encendiendo televisor..."
+        return "Turning on TV..."
 
-# Uso: X.X.X.X:5000/off
-@app.route('/off') # Apaga el televisor por medio del cec
+# Usage: X.X.X.X:5000/off
+@app.route('/off') # Turns off the TV using CEC  
 def off():
         os.system('echo "standby 0" | cec-client -s -d 1')
-        return "Apagando televisor..."
+        return "Turning off TV..."
 
-# Uso: X.X.X.X:5000/web?url=*enlace*
-@app.route('/web') # Abre una página web en la RaspB
+# Usage: X.X.X.X:5000/web?url=*link*
+@app.route('/web') # Opens a web page on the Raspberry Pi  
 def web():
         url = request.args['url']
         os.system(f'sensible-browser {url} --start-fullscreen')
-        return "Abriendo Web..."
+        return "Opening Web..."
 
-# Uso: X.X.X.X:5000/update
-@app.route('/update') # Permite la actualización mediante github
+# Usage: X.X.X.X:5000/update
+@app.route('/update') # Allows updating via GitHub  
 def update():
-        os.system(f"git pull https://github.com/Ki-re/api_controltv.git main") # Clonamos el repositorio    
-        return "Actualización Realizada Correctamente"
+        os.system(f"git pull https://github.com/Ki-re/api_controltv.git main") # Clone the repository    
+        return "Update Successfully Completed"
 
-# Uso: X.X.X.X:5000/as
-@app.route('/as') # Marca la Raspb como source activo
+# Usage: X.X.X.X:5000/as
+@app.route('/as') # Marks the Raspberry Pi as an active source  
 def active_source():
         os.system('echo "as" | cec-client -s -d 1')   
-        return "Active Source correcto"
+        return "Active Source set successfully"
 
-# Uso: X.X.X.X:5000/status
-@app.route('/status') # Marca la Raspb como source activo
+# Usage: X.X.X.X:5000/status
+@app.route('/status') # Checks the power status of the TV  
 def status():
         status = os.popen('echo "pow 0.0.0.0" | cec-client -s -d 1 | grep "power status"').read()  
         return status
@@ -57,9 +57,9 @@ def status():
 ########################################################################################################################################
 
 os.system("clear")
-print(f"Iniciado Correctamente en la dirección: {ip}:{run_port}")
+print(f"Successfully started at address: {ip}:{run_port}")
 
 @werkzeug.serving.run_with_reloader
-def run_server(): # Contempla la actualización de los archivos para el correcto funcionamiento del /update
+def run_server(): # Handles file updates for proper /update functionality  
         app.debug = True
-        serve(app, host=ip, port=run_port) # Ejecuta por medio de waitress
+        serve(app, host=ip, port=run_port) # Runs via Waitress
